f676658090dc463e4bf3fa602e80bbfc0fda3f8a
ManifoldFR <wilson.jallet@polytechnique.org>
2024-02-20 19:21:58 +0100

    analysis: switch to polars

--- analysis.py
+++ analysis.py
@@ -1,16 +1,24 @@
 import marimo
 
-__generated_with = "0.2.4"
-app = marimo.App()
+__generated_with = "0.2.5"
+app = marimo.App(width="full")
 
 
 @app.cell
 def __():
     import orjson
-    import pandas as pd
+    import polars as pl
     import numpy as np
     import matplotlib.pyplot as plt
-    return np, orjson, pd, plt
+    return np, orjson, pl, plt
+
+
+@app.cell
+def __():
+    import hvplot.polars
+
+    hvplot.extension('matplotlib')
+    return hvplot,
 
 
 @app.cell
@@ -19,13 +27,8 @@ def __(orjson):
         bench_data = orjson.loads(f.read())
 
     bench_data = bench_data["benchmarks"]
-    return bench_data, f
-
-
-@app.cell
-def __(bench_data):
     bench_data
-    return
+    return bench_data, f
 
 
 @app.cell
@@ -43,7 +46,6 @@ def __():
             "threads": int(th),
             "time": entry["real_time"]
         }
-
     return extract, re
 
 
@@ -55,29 +57,60 @@ def __(bench_data, extract):
 
 
 @app.cell
-def __(pd, processed_):
-    df = pd.DataFrame.from_dict(processed_)
-    df.set_index(["b", "threads", "N"], inplace=True)
+def __(pl, processed_):
+    df = pl.from_dicts(processed_)
+    df = df.with_columns((pl.col("time") * pl.col("threads")).alias("scaled"))
     df
     return df,
 
 
 @app.cell
-def __(df):
-    dfc_ = df.loc["contigs"].reset_index(level=1)
-    dfc_
-    return dfc_,
+def __(df, pl, plt):
+    _dfc = df.filter(pl.col("threads") == 2).drop("threads")
+    _fig, _ax = plt.subplots()
+
+    for (_name,), _sdf in _dfc.partition_by(["b"], as_dict=True).items():
+        _ax.plot(_sdf["N"], _sdf["time"], label=_name)
+
+    _ax.legend()
+    _ax.set_ylabel("time")
+    _ax.set_xlabel("N")
+    _ax
+    return
 
 
 @app.cell
-def __(dfc_, plt):
-    fig, ax = plt.subplots()
-    for name, group in dfc_.groupby("threads"):
-        group.plot(x="N", y="time", ax=ax, label="{:d} threads".format(name))
-    ax.set_title("Time (contig. struct)")
-    ax
+def __(df, pl):
+    _grp = df.filter(pl.col("threads") == 1).group_by(["b", "N"])
+    _grp.first()
+    return
 
-    return ax, fig, group, name
+
+@app.cell
+def __(df, plt):
+    _fig, _ax = plt.subplots(figsize=(8,6))
+
+    lss = {1: "-", 2:"dotted", 4:"-.", 6:"--"}
+    colors = {
+        "basic": "tab:blue",
+        "contigs": "tab:orange",
+        "veccontigs": "tab:green"
+    }
+
+    for (name,th), _sdf in df.partition_by(["b", "threads"], as_dict=True).items():
+        _ax.plot(_sdf["N"], _sdf["scaled"],
+                 ls=lss[th], color=colors[name],
+                 label="{} ({}c)".format(name, th))
+
+    _ax.legend()
+    _ax.set_ylabel("Time $\\times$ threads [µs]")
+    _ax.set_xlabel("$N$")
+    _ax.set_yscale("log")
+    _ax.set_xscale("log")
+    _ax.set_title("Scaled time vs. workload vs. threads")
+    _fig.savefig("time-vs-size-vs-threads.png")
+    _ax
+    return colors, lss, name, th
 
 
 if __name__ == "__main__":
